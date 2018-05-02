f620e8fa2b5d2adf6f3cd6b415896c884591fa13
John Rykken <john.rykken@gmail.com>
2018-05-02 13:52:22 -0700

    Build functionality checking and unchecking boxes.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
     api_call = Book.new
     @response = api_call.API_call()
     @books = Book.all
+    @available = Book.where(available: true)
+    @unavailable = Book.where(available: false)
   end
 
   def new
@@ -14,9 +16,28 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.available = true
     @book.save
-    if @book.save
-      flash[:notice] = "#{@book.title} was added"
+    respond_to do |format|
+      format.html { redirect_to books_path }
+      format.js
     end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     respond_to do |format|
       format.html { redirect_to books_path }
       format.js
